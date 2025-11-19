15e9347442251d361bf79a34e1b2b2e9a864b5db
Subrat053 <162916520+Subrat053@users.noreply.github.com>
2025-11-19 18:14:21 +0530

    Change CORS local origin port from 3000 to 5000

--- Mail/server.js
+++ Mail/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? process.env.BASE_URL 
-    : ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    : ['http://localhost:5000', 'http://127.0.0.1:5000'],
   credentials: true
 }));
 app.use(express.json());
